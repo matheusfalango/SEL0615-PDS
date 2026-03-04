3600b1e7e4d0476198e45061164a4ce38ec021db
Matheus Falango <matheusfalango12@gmail.com>
2026-03-03 22:21:20 -0300

    Exercício 1 compilado - Tempo discreto

--- ex1.py
+++ ex1.py
@@ -1,20 +1,28 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # type: ignore
 
 # equivalente ao clear, close all, clc
 plt.close('all')
 
-N = 150000  # número de amostras muito elevado (quase contínuo)
-P = 1       # número de períodos
-A = 1       # amplitude do sinal
+P = 1                                   # número de períodos
+A = 1                                   # amplitude do sinal
+F = int(input("Freq fundamental: "))    # frequência fundamental
+FS = int(input("Freq amostragem: "))    # frequência de amostragem
+N = int(FS/F)                           # número de amostras
+TS = 1/FS                               # tempo de amostragem
+
+if FS < 2*F:
+    print("Aliasing!")
 
 vet_amostras = np.arange(0, N*P)  # vetor de amostras
-sinal = A * np.sin(2 * np.pi * vet_amostras / N)
+vet_tempo = vet_amostras*TS
+sinal = A * np.sin(2 * np.pi * F * vet_tempo)
 
 plt.figure(1)
-plt.plot(vet_amostras, sinal, 'r', linewidth=2)
+plt.plot(vet_tempo, sinal, 'r', linewidth=2)    # sinal contínuo
+plt.stem(vet_tempo, sinal)                      # sinal discreto
 plt.title('Senoide discreta')
-plt.xlabel('Amostras')
+plt.xlabel('Tempo Discreto')
 plt.ylabel('Amplitude')
 plt.grid(True)
 
